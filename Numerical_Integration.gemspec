423e3f9fc8e9e58594386163292bd5f1dadc7ada
ksevenCh <ksevenCh@gmail.com>
2023-06-03 21:20:38 +0300

    функция для численной интеграции

---
+++ Numerical_Integration.gemspec
@@ -0,0 +1,7 @@
+Gem::Specification.new do |s|
+  s.authors = ["Kseniya Chekomasova","Kseniya Mochalova","Elizaveta Chueva","Anastasia Garkusha"]
+  s.files = Dir["lib/**/*.rb"]
+  s.name = "Numerical_Integration"
+  s.summary = "Library with methods for numerical integration"
+  s.version = "0.0.1"
+end
---
+++ lib/Numerical_Integration.rb
@@ -0,0 +1,50 @@
+
+  #Метод левых прямоугольников
+  def rectangle_method(function, a, b, n)
+    h = (b - a).to_f / n #Шаг
+    sum = 0.0
+    (0..n) each do |i|
+      x = a + i*h
+      rectangle = function.call(x)
+      sum += rectangle
+    end
+    sum *= h
+    return sum
+  end
+
+  # Метод трапеций
+  def trapezoidal_method(function, a, b, n)
+    h = (b - a).to_f / n 
+    sum = 0.0
+    sum += (function.call(a) + function.call(b)) / 2.0
+    (1..(n-1)) each do |i|
+      sum += function.call(a + i*h)
+    end
+    sum *= h
+    return sum
+  end
+
+  # Формула Симпсона
+  def simpsons_method(function, a, b, n)
+    h = (b - a).to_f / n
+    x = Array.new
+    fx = Array.new
+    
+    (0..n) each do |i|
+      x.push(a + i*h)
+      fx.push(function.call(x[i]))
+    end
+    
+    result = 0.0
+    (0..n) each do |i|
+      if i == 0 or i == n
+        result += fx[i]
+      elsif i % 2 != 0
+        result += 4* fx[i]
+      else
+        result += 2 * fx[i]
+      end
+    end
+    result = result * (h / 3)
+    return result
+  end
